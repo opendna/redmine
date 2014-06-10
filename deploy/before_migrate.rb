b8ea73530edf287991215e52a0bcdfa447ddb1ac
andoyuji <ando@open-dna.jp>
2014-06-10 14:10:51 +0900

    sharedpathを修正

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -3,10 +3,10 @@ if !File.exist?("/usr/bin/rake") then
   rakecmd = 'rake19'
 end
 
-if !File.exist?("#{sharedpath}/config/secrettoken.rb") then
+if !File.exist?("#{shared_path}/config/secrettoken.rb") then
   run "bundle install"
   run "#{rakecmd} generatesecrettoken"
-  run "mv #{releasepath}/config/initializers/secrettoken.rb #{sharedpath}/config/secrettoken.rb"
+  run "mv #{releasepath}/config/initializers/secrettoken.rb #{shared_path}/config/secrettoken.rb"
 end
 
-run "ln -s #{sharedpath}/config/secrettoken.rb #{releasepath}/config/initializers/secrettoken.rb"
+run "ln -s #{shared_path}/config/secrettoken.rb #{releasepath}/config/initializers/secrettoken.rb"
